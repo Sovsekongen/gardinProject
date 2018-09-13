e135012604b18dbd9bcc7c5f36381d6183e59c6b
Sovsekongen <vikpo@live.dk>
2018-09-13 11:39:29 +0200

    Installed mariadb and sql node js support

--- hello-world.js
+++ hello-world.js
@@ -1,6 +1,9 @@
 const addon = require('./build/Release/addon');
 const fs = require("fs");
 const pShell = require("python-shell");
+const mysql = require('mysql');
+const mariadb = require("mariadb");
+
 require('console-stamp')(console, '[HH:MM:ss.l]');
 
 var express = require('express');
@@ -9,7 +12,7 @@ var app = express();
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-   res.sendFile('index.html', {root: __dirname + "/public/templates/"});
+   res.sendFile('phpSQLC.html', {root: __dirname + "/public/templates/"});
 });
 
 app.get('/open', function (req, res) {
@@ -34,3 +37,24 @@ var server = app.listen(8081, "192.168.1.67", function () {
 
    console.log("Example app listening at http://%s:%s", host, port);
 });
+
+var con = mysql.createConnection(
+{
+  host: "192.168.1.67",
+  port: "3306",
+  user: "viktorpi",
+  password: "Preacher-123",
+  database: "gardinProject"
+});
+
+con.connect(function(err) 
+{
+  if (err) throw err;
+  console.log("Connected!");
+  con.query("SELECT value FROM sensor;", function (err, result, fiels) {
+    if (err) throw err;
+      console.log(result);
+});
+	
+});
+
